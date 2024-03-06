67dd7daf1d939bcdabbdcc5ce74e540ab640d8bf
Tayllana Wislla <tayllanawislla@gmail.com>
2024-03-06 10:34:44 -0300

    Versão 3

--- src/Loja_instrumentos/Inventory.java
+++ src/Loja_instrumentos/Inventory.java
@@ -19,40 +19,22 @@ public class Inventory {
 		guitarList.remove(guitar);
 	}
 
-    public void search(GuitarSpec searchSpec) {
+    public List<Guitar> search(GuitarSpec searchSpec) {
         List<Guitar> matchingGuitars = new ArrayList<>();
 
         for (Guitar guitar : guitarList) {
-            GuitarSpec guitarSpec = guitar.getSpec();
-
-            if (searchSpec.getBuilder() != null && !searchSpec.getBuilder().equals(guitarSpec.getBuilder())) {
-                continue;
-            }
-
-            if (searchSpec.getModel() != null && !searchSpec.getModel().equals(guitarSpec.getModel())) {
-                continue;
-            }
-
-            if (searchSpec.getType() != null && !searchSpec.getType().equals(guitarSpec.getType())) {
-                continue;
-            }
-
-            if (searchSpec.getBackWood() != null && !searchSpec.getBackWood().equals(guitarSpec.getBackWood())) {
-                continue;
-            }
-
-            if (searchSpec.getTopWood() != null && !searchSpec.getTopWood().equals(guitarSpec.getTopWood())) {
-                continue;
-            }
-            matchingGuitars.add(guitar);
-        }
-        if (matchingGuitars.isEmpty()) {
-            System.out.println("OPSS... NADA FOI ENCOTRADO.");
-        } else {
-            System.out.println("\n INSTRUMENTOS EM ESTOQUE:");
-            for (Guitar guitar : matchingGuitars) {
-                System.out.println(guitar);
+            if(guitar.getSpec().matches(searchSpec)) {
+                matchingGuitars.add(guitar);	
             }
         }
+        return matchingGuitars;
+//        if (matchingGuitars.isEmpty()) {
+//            System.out.println("OPSS... NADA FOI ENCOTRADO.");
+//        } else {
+//            System.out.println("\n INSTRUMENTOS EM ESTOQUE:");
+//            for (Guitar guitar : matchingGuitars) {
+//                System.out.println(guitar);
+//            }
+//        }
     }
 }
--- src/Loja_instrumentos/Main.java
+++ src/Loja_instrumentos/Main.java
@@ -1,18 +1,26 @@
 package Loja_instrumentos;
 
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         Inventory inventory = new Inventory(null);
 
-        inventory.addGuitar(new Guitar("324894t45", 999.99, new GuitarSpec(Builder.FENDER, Model.CUSTOM_SHOP, Type.ELECTRIC, Wood.MAHOGANY, Wood.MAPLE)));
-        inventory.addGuitar(new Guitar("56tre6875", 1299.99, new GuitarSpec(Builder.GIBSON, Model.TELECASTER, Type.ELECTRIC, Wood.MAHOGANY, Wood.MAPLE)));
-        inventory.addGuitar(new Guitar("9284fj42w", 1599.99, new GuitarSpec(Builder.GIBSON, Model.D_28, Type.ELECTRIC, Wood.COCOBOLO, Wood.MAPLE)));
+        inventory.addGuitar(new Guitar("324894t45", 999.99, new GuitarSpec(Builder.FENDER, Model.CUSTOM_SHOP, Type.ELECTRIC, Wood.MAHOGANY, Wood.MAPLE, 6)));
+        inventory.addGuitar(new Guitar("56tre6875", 1299.99, new GuitarSpec(Builder.GIBSON, Model.TELECASTER, Type.ELECTRIC, Wood.MAHOGANY, Wood.MAPLE, 7)));
+        inventory.addGuitar(new Guitar("9284fj42w", 1599.99, new GuitarSpec(Builder.GIBSON, Model.D_28, Type.ELECTRIC, Wood.COCOBOLO, Wood.MAPLE, 8)));
 
-        inventory.search(new GuitarSpec(Builder.GIBSON, null, null, null, null));
-       
-        
+        List<Guitar> matchingGuitars = inventory.search(new GuitarSpec(Builder.GIBSON, Model.TELECASTER, Type.ELECTRIC, Wood.MAHOGANY, Wood.MAPLE, 7));
+        if (matchingGuitars.isEmpty()) {
+            System.out.println("OPSS... NADA FOI ENCOTRADO.");
+        } else {
+            System.out.println("\n INSTRUMENTOS EM ESTOQUE:");
+            for (Guitar guitar : matchingGuitars) {
+                System.out.println(guitar);
+            }
+        }
 	}
 
 }
